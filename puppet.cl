81c2f52c797fea030feff19164b0a77f5c38428e
codekidX <ckrackerx15@gmail.com>
2016-12-05 16:24:25 +0530

    command list execution completed :grin:

---
+++ puppet.cl
@@ -0,0 +1,14 @@
+{
+	'ax : 'exit()',
+	'a1': 'sudo apt install software-properties-common && sudo add-apt-repository ppa:philip.scott/elementary-tweaks && sudo apt update && sudo apt install elementary-tweaks -y',
+	'a2': 'sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily && sudo apt-get update && sudo apt-get install elementary-tweaks -y', 
+	'a3': 'sudo apt-add-repository ppa:versable/elementary-update && sudo apt-get update && sudo apt-get install elementary-tweaks -y',
+	'a4': 'sudo apt-get install git -y',
+	'a5': 'sudo apt -y install gnome-disk-utility',
+	'a6': 'sudo add-apt-repository ppa:n-muench/programs-ppa && sudo apt-get update && sudo apt-get install filezilla -y',
+	'a7_1': 'wget -O /tmp/google-chrome-stable_current_i386.deb https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb && sudo dpkg -i /tmp/google-chrome-stable_current_i386.deb',
+	'a7_2': 'wget -O /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb',
+	'a8': 'sudo add-apt-repository -y ppa:webupd8team/sublime-text-3 && sudo apt -y update && sudo apt -y install sublime-text-installer',
+	'a9': '	sudo add-apt-repository -y ppa:webupd8team/atom && sudo apt -y update && sudo apt -y install atom',
+	'a10': 'sudo apt-get update && sudo apt-get install vlc browser-plugin-vlc -y'
+}
--- run.py
+++ run.py
@@ -19,26 +19,28 @@
 ##########################################################################
 
 import os
-import pickle
 import shutil
 import sys
+import ast
 
 #constants
 INTEGRATE_HOME = 'cp run.py ~/'
 COMMANDS_CL_FILE = 'puppet.cl'
-COMMAND_GET_CL 'wget'
+COMMAND_GET_CL = 'wget https://raw.githubusercontent.com/codekidX/linux_nooby_script/master/puppet.cl'
+CL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/" + COMMANDS_CL_FILE
 
 # commands
-COMMAND_E_TWEAKS_LOKI = "sudo apt install software-properties-common && sudo add-apt-repository ppa:philip.scott/elementary-tweaks && sudo apt update && sudo apt install elementary-tweaks -y"
-COMMAND_E_TWEAKS_FREYA = "sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily && sudo apt-get update && sudo apt-get install elementary-tweaks -y"
-COMMAND_E_TWEAKS_LOKI = "sudo apt-add-repository ppa:versable/elementary-update && sudo apt-get update && sudo apt-get install elementary-tweaks -y"
-COMMAND_VLC = "sudo apt-get update && sudo apt-get install vlc browser-plugin-vlc -y"
-COMMAND_GIT = "sudo apt-get install git -y"
-COMMAND_FILEZILLA = "sudo add-apt-repository ppa:n-muench/programs-ppa && sudo apt-get update && sudo apt-get install filezilla -y"
+# COMMAND_E_TWEAKS_LOKI = "sudo apt install software-properties-common && sudo add-apt-repository ppa:philip.scott/elementary-tweaks && sudo apt update && sudo apt install elementary-tweaks -y"
+# COMMAND_E_TWEAKS_FREYA = "sudo add-apt-repository ppa:mpstark/elementary-tweaks-daily && sudo apt-get update && sudo apt-get install elementary-tweaks -y"
+# COMMAND_E_TWEAKS_LOKI = "sudo apt-add-repository ppa:versable/elementary-update && sudo apt-get update && sudo apt-get install elementary-tweaks -y"
+# COMMAND_VLC = "sudo apt-get update && sudo apt-get install vlc browser-plugin-vlc -y"
+# COMMAND_GIT = "sudo apt-get install git -y"
+# COMMAND_FILEZILLA = "sudo add-apt-repository ppa:n-muench/programs-ppa && sudo apt-get update && sudo apt-get install filezilla -y"
 
 
 def main():
-	# before os subroutine is called cl [commands-list] extraction subroutine is called to dump the command list
+	# before script subroutine is called cl [commands-list] extraction subroutine is called to dump the command list
+	get_cl()
 	# ask users if they want to copy run.py to home directory
 	home_system_integration()
 
@@ -64,7 +66,7 @@ def register_menu_choice():
 	os_choice = raw_input('Enter Number [1-3] or [x]: ')
 
 	if os_choice == '1':
-		show_apt_menu()
+		apt_subroutine()
 	elif os_choice == '2':
 		show_eos_menu()
 	elif os_choice == 'x':
@@ -85,7 +87,17 @@ def command(the_command):
 	os.system(the_command)
 
 def get_cl():
-	pass
+	print 'Please wait ... '
+	print '%s' % (is_cl_present())
+	print 'Getting updated commands list'
+	if is_cl_present() is True:
+		pass
+	else:
+		command(COMMAND_GET_CL)
+
+	command('clear')
+	print 'Commands list fetched !'
+
 
 ####################################################################
 #                                                                  #
@@ -97,11 +109,9 @@ def os_subroutine():
 	show_main_menu()
 	register_menu_choice()
 
-def ubuntu_subroutine():
-	show_ubuntu_menu()
-
-def eos_subroutine():
-	show_eos_menu()
+def apt_subroutine():
+	show_apt_menu()
+	register_apt_choice()
 
 def arch_subroutine():
 	show_arch_menu()
@@ -136,6 +146,23 @@ def show_apt_menu():
 	print ' -------------------------------------------------------- '
 	print ' x. Exit'
 
+def register_apt_choice():
+	choices = {}
+	apt_choice = raw_input('Enter Number or [x] \{Multiple installs by inserting commas\}: ')
+
+	# if choice_type == True then the user has entered multiple input
+	choice_type = single_or_multiple(apt_choice)
+
+	if choice_type is True:
+		choices = apt_choice.split(',')
+		for i in range(0, len(choices)):
+			execute_command('a', choices[i])
+	else:
+		execute_command('a', apt_choice)
+
+	# show menu agian after all execution complete
+	show_apt_menu()
+
 ####################################################################
 #                                                                  #
 #                         ARCH STUFFS                              #
@@ -166,20 +193,46 @@ def show_arch_menu():
 # - of installing any stuff                                        #
 ####################################################################
 
-def register_command(the_choice):
+def execute_command(the_pm, the_choice):
 	# Prefixes
-	# u - ubuntu choices
-	# e - eos choices
-	# a - arch choices
-	pass
+	# a - ubuntu choices
+	# p - arch choices
+
+	# first load the file to get commands in the form of dictionary
+	cl_file = open(CL_PATH, 'r')
+	# read cl file
+	cl_str = cl_file.read()
+	# file contents are in the form of <'str'> --> convert it to <'dict'>
+	cl_dict = ast.literal_eval(cl_str)
+	# evaluate choice as dictionary key :D
+	cl_choice = the_pm + the_choice
+	# print(cl_dict[cl_choice])
+	#                 Choice Restrictor
+	#   -- if there is a os specific choice [append suffix]
+	# ------------------------------------------------------
+	#
+	if cl_choice is 'a7':
+		if is_os_64bit() == True:
+			cl_choice.append('_2')
+		else:
+			cl_choice.append('_1')
+
+
+	# all hail the mighty script execute_command starts NOW !!!
+	command(cl_dict[cl_choice])
+	# clear screen after each command
+	command('clear')
 
 def is_cl_present():
 	# get the current directory of the
-	curr_dir = os.path.dirname(os.path.abspath(__file__))
-	return os.path.exists(curr_dir + "/" + COMMANDS_CL_FILE)
+	return os.path.exists(CL_PATH)
 
 def is_os_64bit():
     return platform.machine().endswith('64')
+
+def single_or_multiple(choice):
+	return ',' in choice
+	
 
 if __name__ == '__main__':
 	main()
